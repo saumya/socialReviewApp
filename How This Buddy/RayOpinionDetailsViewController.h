741a47b4af8f51d7593a36e2b1483ea40b026882
saumya <2saumyaray@gmail.com>
2015-01-17 20:16:54 +0530

    wip, view customisation

---
+++ How This Buddy/RayOpinionDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  RayOpinionDetailsViewController.h
+//  How This Buddy
+//
+//  Created by saumya ray on 1/17/15.
+//  Copyright (c) 2015 @saumya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RayOpinionDetailsViewController : UIViewController
+
+@end
